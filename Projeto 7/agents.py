60057ac26db665d64bccd0b54e257f9608963a3c
GeorgeJuniorGG <g216741@dac.unicamp.br>
2022-06-09 17:50:53 -0300

    Primeira versao dos codigos

---
+++ Projeto 7/agents.py
@@ -0,0 +1,418 @@
+from collections import defaultdict
+from environment_simulator import mdp
+
+# Calcula pelo metodo do ponto fixo
+def policyEvaluation(policy, utility, gamma, rewards, Prob, states, reverseStates):
+    n = 25                          # Numero de iteracoes que determinei empiricamente para a aproximacao
+    for i in range (n):
+        for j in range(11):
+            if(policy[j] == 0):
+                direction = "left"
+            elif(policy[j] == 1):
+                direction = "right"
+            elif(policy[j] == 3):
+                direction = "down"
+            else:
+                direction = "up"
+            utilityAux = rewards[states[j]]
+
+            for dic in Prob[states[j]][direction]:
+                newS = dic
+                prob = Prob[states[j]][direction][newS]
+                utilityAux += gamma * prob * utility[reverseStates[newS]]
+
+            utility[j] = utilityAux
+
+    return utility
+
+def directUtilityEstimation (policy, environment, states, reverseStates):
+
+    utilidade = [0, 0, 0,
+                 0, 0,
+                 0, 0, 0,
+                 0, 0, 0]
+    
+    happened =  [1, 1, 1,    # Vetor que indica se ja foi encontrado algum valor de utilidade para os estados
+                 1, 1,
+                 1, 1, 1,
+                 1, 1, 1]
+    
+    for i in range (200):
+        count = 0
+        hist_estados = []        # Guarda os estados visitados
+        hist_recompensas = []    # Guarda as recompensas recebidas
+
+        utilidadeAux = [10, 10, 10, # Valores dummies para indicar que nao foi setado nenhum valor
+                 10, 10,
+                 10, 10, 10,
+                 10, 10, 10]
+        
+        ocorrencias =  [0, 0, 0,
+                        0, 0,
+                        0, 0, 0,
+                        0, 0, 0]         # Conta se algum estado foi alcancado mais de uma vez
+
+        estado = (1,1)
+        recompensa = -0.04
+
+        hist_estados.append(estado)
+        hist_recompensas.append(recompensa)
+        ocorrencias[reverseStates[estado]] += 1
+
+        while (estado not in environment.terminalStates):
+            aux = policy[reverseStates[estado]]
+            if(aux == 0):
+                direction = "left"
+            elif (aux == 1):
+                direction = "right"
+            elif (aux == 3):
+                direction = "down"
+            else:
+                direction = "up"
+
+            estado, recompensa = environment.getDestination(estado, direction)
+            hist_estados.append(estado)
+            hist_recompensas.append(recompensa)
+            ocorrencias[reverseStates[estado]] += 1
+            count += 1
+            if count > 1000:
+                return -1
+
+        for j in range(len(hist_estados)):
+            if(utilidadeAux[reverseStates[hist_estados[j]]] == 10):
+                utilidadeAux[reverseStates[hist_estados[j]]] = sum(hist_recompensas[j:])
+            else:
+                utilidadeAux[reverseStates[hist_estados[j]]] += sum(hist_recompensas[j:])
+        
+        for j in range(11):
+            if(utilidadeAux[j] == 10):
+                utilidadeAux[j] = 0
+            elif(ocorrencias[j] > 1):
+                utilidadeAux[j] = utilidadeAux[j] / ocorrencias[j]
+ 
+        for j in range(11):
+            if(utilidadeAux[j] != 0):
+                if(happened[j] == 0):
+                    utilidade[j] = (utilidade[j] + utilidadeAux[j]) /2
+                else:
+                    utilidade[j] = utilidadeAux[j]
+                    happened[j] = 0
+
+    return utilidade
+
+def TD (policy, environment, states, reverseStates, gamma):
+    utilidade = [0, 0, 0,
+                 0, 0,
+                 0, 0, 0,
+                 0, 0, 0]
+
+    N_estado = [0, 0, 0,
+                 0, 0,
+                 0, 0, 0,
+                 0, 0, 0]
+    
+    # De acordo com o livro
+    alfa = lambda n: 60./(59+n)
+
+    for i in range(200):
+        count = 0
+        estado = (1,1)
+        recompensa = -0.04
+        prevRecompensa = 10
+        prevEstado = -1
+
+        if(N_estado[reverseStates[estado]] == 0):
+            utilidade[reverseStates[estado]] = recompensa
+        
+        if(prevEstado != -1):
+            N_estado[reverseStates[prevEstado]] += 1
+            utilidade[reverseStates[prevEstado]] += alfa(N_estado[reverseStates[prevEstado]]) * (prevRecompensa + gamma * utilidade[reverseStates[estado]] - utilidade[reverseStates[prevEstado]])
+
+        prevEstado = estado
+        prevRecompensa = recompensa
+
+        while(estado not in environment.terminalStates):
+
+            aux = policy[reverseStates[estado]]
+            if(aux == 0):
+                direction = "left"
+            elif (aux == 1):
+                direction = "right"
+            elif (aux == 3):
+                direction = "down"
+            else:
+                direction = "up"
+
+            estado, recompensa = environment.getDestination(estado, direction)
+
+            if(N_estado[reverseStates[estado]] == 0):
+                utilidade[reverseStates[estado]] = recompensa
+        
+            if(prevEstado != -1):
+                N_estado[reverseStates[prevEstado]] += 1
+                utilidade[reverseStates[prevEstado]] += alfa(N_estado[reverseStates[prevEstado]]) * (prevRecompensa + gamma * utilidade[reverseStates[estado]] - utilidade[reverseStates[prevEstado]])
+
+            if (estado not in environment.terminalStates):
+                prevEstado = estado
+                prevRecompensa = recompensa
+                
+            else:
+                prevEstado = -1
+                prevRecompensa = 10
+
+            count += 1
+            if count > 1000:
+                return -1
+
+    return utilidade
+
+def ADP (policy, environment, states, reverseStates, gamma):
+    utilidade = [0, 0, 0,
+                 0, 0,
+                 0, 0, 0,
+                 0, 0, 0]
+    visitados = set()
+    N_prevEstado_prevDir = defaultdict(int)
+    N_estado_prevEstado_prevDir = defaultdict(int)
+    
+    P = {(1,1):{"left": {}, 
+                "right": {}, 
+                "up": {}, 
+                "down": {}}, 
+         (1,2):{"left": {}, 
+                "right": {}, 
+                "up": {}, 
+                "down": {}}, 
+        (1,3): {"left": {}, 
+                "right": {}, 
+                "up": {}, 
+                "down": {}},
+        (2,1): {"left": {}, 
+                "right": {}, 
+                "up": {}, 
+                "down": {}}, 
+        (2,2): {"left": {}, 
+                "right": {}, 
+                "up": {}, 
+                "down": {}}, 
+        (2,3): {"left": {}, 
+                "right": {}, 
+                "up": {}, 
+                "down": {}}, 
+        (3,1): {"left": {}, 
+                "right": {}, 
+                "up": {}, 
+                "down": {}}, 
+        (3,2): {"left": {}, 
+                "right": {}, 
+                "up": {}, 
+                "down": {}}, 
+        (3,3): {"left": {}, 
+                "right": {}, 
+                "up": {}, 
+                "down": {}},
+        (4,1): {"left": {}, 
+                "right": {}, 
+                "up": {}, 
+                "down": {}}, 
+        (4,2): {"left": {}, 
+                "right": {}, 
+                "up": {}, 
+                "down": {}}, 
+        (4,3): {"left": {}, 
+                "right": {}, 
+                "up": {}, 
+                "down": {}}}
+
+    R = {(1,1): 0, (1,2): 0, (1,3): 0,
+         (2,1): 0, (2,2): 0, (2,3): 0,
+         (3,1): 0, (3,2): 0, (3,3): 0,
+         (4,1): 0, (4,2): 0, (4,3): 0}
+    
+    for i in range(200):
+        count = 0
+        estado = (1,1)
+        prevEstado = -1
+        recompensa = -0.04
+        prevDirecao = -1
+
+        if (estado not in visitados):
+            visitados.add(estado)
+            utilidade[reverseStates[estado]] = recompensa
+            R[estado] = recompensa
+
+        if(prevEstado != -1):
+            N_prevEstado_prevDir[(prevEstado, prevDirecao)] += 1
+            N_estado_prevEstado_prevDir[(estado, prevEstado, prevDirecao)] += 1
+            # Para cada t tal que N estado|prevEstado prevDir [t, prevEstado, prevDir] seja diferente de 0
+            for j in [estAtual for (estAtual, estAnt, dirAnt), ocorrencias in N_estado_prevEstado_prevDir.items() if ocorrencias != 0 and (estAnt, dirAnt) == (prevEstado, prevDirecao)]:
+                P[prevEstado][prevDirecao][j] = N_estado_prevEstado_prevDir[(j, prevEstado, prevDirecao)] / N_prevEstado_prevDir[(prevEstado, prevDirecao)]
+
+        
+        utilidade = policyEvaluation(policy, utilidade, gamma, R, P, states, reverseStates)
+        
+        prevEstado = estado
+        aux = policy[reverseStates[estado]]
+        if(aux == 0):
+            prevDirecao = "left"
+        elif (aux == 1):
+            prevDirecao = "right"
+        elif (aux == 3):
+            prevDirecao = "down"
+        else:
+            prevDirecao = "up"
+
+        while(estado not in environment.terminalStates):
+
+            aux = policy[reverseStates[estado]]
+            if(aux == 0):
+                direction = "left"
+            elif (aux == 1):
+                direction = "right"
+            elif (aux == 3):
+                direction = "down"
+            else:
+                direction = "up"
+
+            estado, recompensa = environment.getDestination(estado, direction)
+
+            if (estado not in visitados):
+                visitados.add(estado)
+                utilidade[reverseStates[estado]] = recompensa
+                R[estado] = recompensa
+
+            if(prevEstado != -1):
+                N_prevEstado_prevDir[(prevEstado, prevDirecao)] += 1
+                N_estado_prevEstado_prevDir[(estado, prevEstado, prevDirecao)] += 1
+                # Para cada t tal que N estado|prevEstado prevDir [t, prevEstado, prevDir] seja diferente de 0
+                for j in [estAtual for (estAtual, estAnt, dirAnt), ocorrencias in N_estado_prevEstado_prevDir.items() if ocorrencias != 0 and (estAnt, dirAnt) == (prevEstado, prevDirecao)]:
+                    P[prevEstado][prevDirecao][j] = N_estado_prevEstado_prevDir[(j, prevEstado, prevDirecao)] / N_prevEstado_prevDir[(prevEstado, prevDirecao)]
+   
+            
+            utilidade = policyEvaluation(policy, utilidade, gamma, R, P, states, reverseStates)
+            if (estado not in environment.terminalStates):
+                prevEstado = estado
+                aux = policy[reverseStates[estado]]
+                if(aux == 0):
+                    prevDirecao = "left"
+                elif (aux == 1):
+                    prevDirecao = "right"
+                elif (aux == 3):
+                    prevDirecao = "down"
+                else:
+                    prevDirecao = "up"
+            else:
+                prevEstado = -1
+                prevDirecao = -1
+
+            count += 1
+            if count > 1000:
+                return -1
+
+    return utilidade
+
+environment = mdp((1,1))
+
+# Valor de gamma definido com base no que foi feito no ultimo exercicio
+gamma = 1
+
+# Mapeamento de identificadores de estados
+# OBS: o estado (2,2) nao esta presente pois ele corresponde a uma parede
+states =   {0: (1,1), 1: (1,2), 2: (1,3), 
+            3: (2,1), 4: (2,3),
+            5: (3,1), 6: (3,2), 7:(3,3),
+            8: (4,1), 9: (4,2), 10: (4,3)}
+reverseStates ={(1,1): 0, (1,2): 1, (1,3):2, 
+                (2,1): 3, (2,3): 4,
+                (3,1): 5, (3,2): 6, (3,3): 7,
+                (4,1): 8, (4,2): 9, (4,3): 10}
+
+# Vetor de acoes
+# 0 corresponde a ir para a esquerda, 1 para a direita, 2 para cima e 3 para baixo
+actions = [0, 1, 2, 3]
+
+# Vetor de política ótima (encontrado no exercicio anterior)
+# Em cada posição, temos a politica para cada estado: 0 corresponde a ir para a esquerda, 1 para a direita, 2 para cima e 3 para baixo
+# OBS: Nao existe politica para os estados terminais porque eles ja sao o final do problema
+optimalPolicy = [2, 2, 1,
+                 0,    1,
+                 0, 2, 1,
+                 0, 0, 0]
+
+# Vetores de políticas aleatórias (apenas 2, como definido no enunciado)
+randomPolicy1 = [2, 0, 1,
+                 1,    3,
+                 2, 0, 1,
+                 1, 0, 0]
+randomPolicy2 = [3, 0, 0,
+                 2,    3,
+                 1, 2, 2,
+                 0, 0, 0] 
+
+# Vetores de utilidade: um para cada algoritmo, para cada politica usada
+utility1a = [0, 0, 0,
+           0, 0,
+           0, 0, 0,
+           0, -1, 1]
+utility1b = [0, 0, 0,
+           0, 0,
+           0, 0, 0,
+           0, -1, 1]
+utility1c = [0, 0, 0,
+           0, 0,
+           0, 0, 0,
+           0, -1, 1]
+
+utility2a = [0, 0, 0,
+           0, 0,
+           0, 0, 0,
+           0, -1, 1]
+utility2b = [0, 0, 0,
+           0, 0,
+           0, 0, 0,
+           0, -1, 1]
+utility2c = [0, 0, 0,
+           0, 0,
+           0, 0, 0,
+           0, -1, 1]
+
+utility3a = [0, 0, 0,
+           0, 0,
+           0, 0, 0,
+           0, -1, 1]
+utility3b = [0, 0, 0,
+           0, 0,
+           0, 0, 0,
+           0, -1, 1]
+utility3c = [0, 0, 0,
+           0, 0,
+           0, 0, 0,
+           0, -1, 1]
+
+# Chamadas dos metodos para as 3 politicas
+
+utility1a = directUtilityEstimation(optimalPolicy, environment, states, reverseStates)
+utility2a = TD(optimalPolicy, environment, states, reverseStates, gamma)
+utility3a = ADP(optimalPolicy, environment, states, reverseStates, gamma)
+
+print(utility1a)
+print(utility2a)
+print(utility3a)
+print("========================================================")
+
+utility1b = directUtilityEstimation(randomPolicy1, environment, states, reverseStates)
+utility2b = TD(randomPolicy1, environment, states, reverseStates, gamma)
+utility3b = ADP(randomPolicy1, environment, states, reverseStates, gamma)
+
+print(utility1b)
+print(utility2b)
+print(utility3b)
+print("========================================================")
+
+utility1c = directUtilityEstimation(randomPolicy2, environment, states, reverseStates)
+utility2c = TD(randomPolicy2, environment, states, reverseStates, gamma)
+utility3c = ADP(randomPolicy2, environment, states, reverseStates, gamma)
+
+print(utility1c)
+print(utility2c)
+print(utility3c)
---
+++ Projeto 7/environment_simulator.py
@@ -0,0 +1,148 @@
+from collections import defaultdict
+import os
+
+class mdp:
+
+    def __init__(self, initialState) -> None:
+        self.initialState = initialState
+        self.terminalStates = ((4,2), (4,3))
+        self.actions = {(1,1): {"left": (((1,1), 0.9), ((1,2), 0.1)), 
+                                "right": (((2,1), 0.8), ((1,2), 0.1), ((1,1), 0.1)), 
+                                "up": (((1,2), 0.8), ((2,1), 0.1), ((1,1), 0.1)), 
+                                "down": (((1,1), 0.9), ((2,1), 0.1))}, 
+                        (1,2): {"left": (((1,2), 0.8), ((1,1), 0.1), ((1,3), 0.1)), 
+                                "right": (((1,2), 0.8), ((1,1), 0.1), ((1,3), 0.1)), 
+                                "up": (((1,2), 0.2), ((1,3), 0.8)), 
+                                "down": (((1,2), 0.2), ((1,1), 0.8))}, 
+                        (1,3): {"left": (((1,3), 0.9), ((1,2), 0.1)), 
+                                "right": (((1,3), 0.1), ((2,3), 0.8), ((1,2), 0.1)), 
+                                "up": (((1,3), 0.9), ((2,3), 0.1)), 
+                                "down": (((1,3), 0.1), ((2,3), 0.1), ((1,2), 0.8))},
+                        (2,1): {"left": (((2,1), 0.2), ((1,1), 0.8)), 
+                                "right": (((2,1), 0.2), ((3,1), 0.8)), 
+                                "up": (((2,1), 0.8), ((1,1), 0.1), ((3,1), 0.1)), 
+                                "down": (((2,1), 0.8), ((1,1), 0.1), ((3,1), 0.1))}, 
+                        (2,2): {"left": 0, 
+                                "right": 0, 
+                                "up": 0, 
+                                "down": 0}, 
+                        (2,3): {"left": (((2,3), 0.2), ((1,3), 0.8)), 
+                                "right": (((2,3), 0.2), ((3,3), 0.8)), 
+                                "up": (((2,3), 0.8), ((1,3), 0.1), ((3,3), 0.1)), 
+                                "down": (((2,3), 0.8), ((1,3), 0.1), ((3,3), 0.1))}, 
+                        (3,1): {"left": (((3,1), 0.1), ((3,2), 0.1), ((2,1), 0.8)), 
+                                "right": (((3,1), 0.1), ((3,2), 0.1), ((4,1), 0.8)), 
+                                "up": (((3,2), 0.8), ((2,1), 0.1), ((4,1), 0.1)), 
+                                "down": (((3,1), 0.8), ((2,1), 0.1), ((4,1), 0.1))}, 
+                        (3,2): {"left": (((3,2), 0.8), ((3,1), 0.1), ((3,3), 0.1)), 
+                                "right": (((4,2), 0.8), ((3,1), 0.1), ((3,3), 0.1)), 
+                                "up": (((3,2), 0.1), ((4,2), 0.1), ((3,3), 0.8)), 
+                                "down": (((3,2), 0.1), ((4,2), 0.1), ((3,1), 0.8))}, 
+                        (3,3): {"left": (((2,3), 0.8), ((3,3), 0.1), ((3,2), 0.1)), 
+                                "right": (((3,2), 0.1), ((4,3), 0.8), ((3,3), 0.1)), 
+                                "up": (((2,3), 0.1), ((4,3), 0.1), ((3,3), 0.8)), 
+                                "down": (((3,2), 0.8), ((2,3), 0.1), ((4,3), 0.1))},
+                        (4,1): {"left": (((4,1), 0.1), ((3,1), 0.8), ((4,2), 0.1)), 
+                                "right": (((4,1), 0.9), ((4,2), 0.1)), 
+                                "up": (((4,2), 0.8), ((4,1), 0.1), ((3,2), 0.1)), 
+                                "down": (((4,1), 0.9), ((3,1), 0.1))}, 
+                        (4,2): {"left": 0, 
+                                "right": 0, 
+                                "up": 0, 
+                                "down": 0}, 
+                        (4,3): {"left": 0, 
+                                "right": 0, 
+                                "up": 0, 
+                                "down": 0}}
+
+        self.rewards = {(1,1): -0.04, (1,2): -0.04, (1,3): -0.04,
+                        (2,1): -0.04, (2,2): -0.04, (2,3): -0.04,
+                        (3,1): -0.04, (3,2): -0.04, (3,3): -0.04,
+                        (4,1): -0.04, (4,2): -1   , (4,3): 1    }
+    
+    # Dadas uma posicao e uma direcao, simula qual sera o proximo estado do agente, retornando tambem a recompensa desse estado
+    def getDestination(self, position, direction):
+        rand = int.from_bytes(os.urandom(8), byteorder="big") / ((1 << 64) - 1)
+
+        possibilities = []
+        for i in self.actions[position][direction]:
+            possibilities.append(i[1])
+
+        previous = 0
+        for i in range(len(possibilities)):
+            if(previous + possibilities[i] >= rand):            # Determina qual sera o novo estado de acordo com o modelo de
+                pos = i                                         # transicao estabelecido
+                break
+            previous += possibilities[i]
+
+        newpos = self.actions[position][direction][pos][0]      # Corresponde ao novo estado do agente
+        return (newpos, self.getReward(newpos))
+    
+    # Dada uma posicao, retorna a recompensa de chegar ate ela
+    def getReward(self, position):
+        return self.rewards[position]
+
+    # Dadas uma posicao e uma direcao, retorna o modelo de transicao
+    def getTransitionProbabilities(self, position, direction):
+        return self.actions[position][direction]
+
+class customMDP(mdp):
+        def __init__(self, initialState):
+                super().__init__(initialState)
+                dict_encadeado = lambda: defaultdict(dict_encadeado)        # Cria dicionario de dicionario, com base no codigo do repositorio oficial do livro
+                self.Prob = dict_encadeado()
+                self.actions = {(1,1): {"left": [[(1,1), 0.9], [(1,2), 0.1]], 
+                                "right": [[(2,1), 0.8], [(1,2), 0.1], [(1,1), 0.1]], 
+                                "up": [[(1,2), 0.8], [(2,1), 0.1], [(1,1), 0.1]], 
+                                "down": [[(1,1), 0.9], [(2,1), 0.1]]}, 
+                        (1,2): {"left": [[(1,2), 0.8], [(1,1), 0.1], [(1,3), 0.1]], 
+                                "right": [[(1,2), 0.8], [(1,1), 0.1], [(1,3), 0.1]], 
+                                "up": [[(1,2), 0.2], [(1,3), 0.8]], 
+                                "down": [[(1,2), 0.2], [(1,1), 0.8]]}, 
+                        (1,3): {"left": [[(1,3), 0.9], [(1,2), 0.1]], 
+                                "right": [[(1,3), 0.1], [(2,3), 0.8], [(1,2), 0.1]], 
+                                "up": [[(1,3), 0.9], [(2,3), 0.1]], 
+                                "down": [[(1,3), 0.1], [(2,3), 0.1], [(1,2), 0.8]]},
+                        (2,1): {"left": [[(2,1), 0.2], [(1,1), 0.8]], 
+                                "right": [[(2,1), 0.2], [(3,1), 0.8]], 
+                                "up": [[(2,1), 0.8], [(1,1), 0.1], [(3,1), 0.1]], 
+                                "down": [[(2,1), 0.8], [(1,1), 0.1], [(3,1), 0.1]]}, 
+                        (2,2): {"left": 0, 
+                                "right": 0, 
+                                "up": 0, 
+                                "down": 0}, 
+                        (2,3): {"left": [[(2,3), 0.2], [(1,3), 0.8]], 
+                                "right": [[(2,3), 0.2], [(3,3), 0.8]], 
+                                "up": [[(2,3), 0.8], [(1,3), 0.1], [(3,3), 0.1]], 
+                                "down": [[(2,3), 0.8], [(1,3), 0.1], [(3,3), 0.1]]}, 
+                        (3,1): {"left": [[(3,1), 0.1], [(3,2), 0.1], [(2,1), 0.8]], 
+                                "right": [[(3,1), 0.1], [(3,2), 0.1], [(4,1), 0.8]], 
+                                "up": [[(3,2), 0.8], [(2,1), 0.1], [(4,1), 0.1]], 
+                                "down": [[(3,1), 0.8], [(2,1), 0.1], [(4,1), 0.1]]}, 
+                        (3,2): {"left": [[(3,2), 0.8], [(3,1), 0.1], [(3,3), 0.1]], 
+                                "right": [[(4,2), 0.8], [(3,1), 0.1], [(3,3), 0.1]], 
+                                "up": [[(3,2), 0.1], [(4,2), 0.1], [(3,3), 0.8]], 
+                                "down": [[(3,2), 0.1], [(4,2), 0.1], [(3,1), 0.8]]}, 
+                        (3,3): {"left": [[(2,3), 0.8], [(3,3), 0.1], [(3,2), 0.1]], 
+                                "right": [[(3,2), 0.1], [(4,3), 0.8], [(3,3), 0.1]], 
+                                "up": [[(2,3), 0.1], [(4,3), 0.1], [(3,3), 0.8]], 
+                                "down": [[(3,2), 0.8], [(2,3), 0.1], [(4,3), 0.1]]},
+                        (4,1): {"left": [[(4,1), 0.1], [(3,1), 0.8], [(4,2), 0.1]], 
+                                "right": [[(4,1), 0.9], [(4,2), 0.1]], 
+                                "up": [[(4,2), 0.8], [(4,1), 0.1], [(3,2), 0.1]], 
+                                "down": [[(4,1), 0.9], [(3,1), 0.1]]}, 
+                        (4,2): {"left": 0, 
+                                "right": 0, 
+                                "up": 0, 
+                                "down": 0}, 
+                        (4,3): {"left": 0, 
+                                "right": 0, 
+                                "up": 0, 
+                                "down": 0}}
+                self.rewards = {(1,1): -0.04, (1,2): -0.04, (1,3): -0.04,
+                        (2,1): -0.04, (2,2): -0.04, (2,3): -0.04,
+                        (3,1): -0.04, (3,2): -0.04, (3,3): -0.04,
+                        (4,1): -0.04, (4,2): -1   , (4,3): 1    }
+
+        def getTransitionProbabilities(self, position, direction):      # Para as probabilidades aprendidas guardadas em P
+            return [(prob, res) for (res, prob) in self.Prob[(position, direction)].items()]
